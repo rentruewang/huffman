afb9a852e94f6c0d0aa08401ace249700a806e16
RenChu Wang <patrick1031wang@gmail.com>
2020-02-16 12:32:51 +0800

    handcraft is a bit faster when not all fields used

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,6 @@
-use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
-use std::collections::{BinaryHeap, HashMap};
-use std::{env, fs};
+use std::{cmp, collections, env, fs};
 
-#[derive(Eq, PartialEq, PartialOrd, Ord)]
+#[derive(cmp::Eq, cmp::PartialEq, cmp::PartialOrd, cmp::Ord)]
 struct Node<K, V> {
     val: V,
     key: K,
@@ -10,11 +8,80 @@ struct Node<K, V> {
     right: Option<Box<Node<K, V>>>,
 }
 
+// struct Node<K, V> {
+//     key: K,
+//     val: V,
+//     left: Option<Box<Node<K, V>>>,
+//     right: Option<Box<Node<K, V>>>,
+// }
+
+// impl<K, V> cmp::Eq for Node<K, V> where V: cmp::Ord {}
+// impl<K, V> cmp::PartialEq for Node<K, V>
+// where
+//     V: cmp::Ord,
+// {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.val == other.val
+//     }
+//     fn ne(&self, other: &Self) -> bool {
+//         self.val != other.val
+//     }
+// }
+
+// impl<K, V> cmp::Ord for Node<K, V>
+// where
+//     V: cmp::Ord,
+// {
+//     fn cmp(&self, other: &Self) -> cmp::Ordering {
+//         if let Some(a) = self.partial_cmp(other) {
+//             a
+//         } else {
+//             unreachable!()
+//         }
+//     }
+//     fn max(self, other: Self) -> Self {
+//         cmp::max(self, other)
+//     }
+//     fn min(self, other: Self) -> Self {
+//         cmp::min(self, other)
+//     }
+//     // fn clamp(self, min: Self, max: Self) -> Self {
+//     //     cmp::min(max, cmp::max(min, self))
+//     // }
+// }
+
+// impl<K, V> cmp::PartialOrd for Node<K, V>
+// where
+//     V: cmp::Ord,
+// {
+//     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+//         if self.lt(other) {
+//             Some(cmp::Ordering::Less)
+//         } else if self.ge(other) {
+//             Some(cmp::Ordering::Greater)
+//         } else {
+//             Some(cmp::Ordering::Equal)
+//         }
+//     }
+//     fn lt(&self, other: &Self) -> bool {
+//         self.val < other.val
+//     }
+//     fn le(&self, other: &Self) -> bool {
+//         self.val <= other.val
+//     }
+//     fn gt(&self, other: &Self) -> bool {
+//         self.val > other.val
+//     }
+//     fn ge(&self, other: &Self) -> bool {
+//         self.val >= other.val
+//     }
+// }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let content: Vec<char> = fs::read_to_string(&args[1]).unwrap().chars().collect();
 
-    let mut word_count: HashMap<char, isize> = HashMap::new();
+    let mut word_count: collections::HashMap<char, isize> = collections::HashMap::new();
 
     for ch in content.into_iter() {
         let v = word_count.get_mut(&ch);
@@ -25,7 +92,7 @@ fn main() {
         }
     }
 
-    let mut pq: BinaryHeap<Node<char, isize>> = BinaryHeap::new();
+    let mut pq: collections::BinaryHeap<Node<char, isize>> = collections::BinaryHeap::new();
 
     for (key, val) in word_count.into_iter() {
         let s = Node {
@@ -66,8 +133,12 @@ fn main() {
 }
 
 // move hash map out of this function after use
-fn traverse(node: Node<char, isize>) -> HashMap<char, String> {
-    fn _traverse(node: Node<char, isize>, map: &mut HashMap<char, String>, path: &String) {
+fn traverse(node: Node<char, isize>) -> collections::HashMap<char, String> {
+    fn _traverse(
+        node: Node<char, isize>,
+        map: &mut collections::HashMap<char, String>,
+        path: &String,
+    ) {
         let left = node.left;
         let right = node.right;
 
@@ -85,7 +156,7 @@ fn traverse(node: Node<char, isize>) -> HashMap<char, String> {
             map.insert(node.key, pc);
         }
     }
-    let mut path_list: HashMap<char, String> = HashMap::new();
+    let mut path_list: collections::HashMap<char, String> = collections::HashMap::new();
     _traverse(node, &mut path_list, &String::from(""));
     path_list
 }
