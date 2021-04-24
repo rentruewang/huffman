4e3d6eb995ee8383ecc3083ece654e87a815a88c
RenChu Wang <patrick1031wang@gmail.com>
2021-04-24 23:42:57 +0800

    🦫 Simplicity is complicated.
    Removed some redundant comments that only get in the way.
    
    Less is sometimes more. People are impatient. How many comment I can take away before someone starts confusing? Probably a lot.

--- huffman.go
+++ huffman.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"container/heap"
-)
+import "container/heap"
 
-// Any can be anything!
+// Any is anything
 type Any = interface{}
 
-// HuffmanNode is actually a normal node, but huffman.
-// It is created to aid the process of creating Huffman trees.
+// HuffmanNode a node in a Huffman tree
 type HuffmanNode struct {
 	// left points to the left HuffmanNode.
 	left *HuffmanNode
 	// right points to the right HuffmanNode.
 	right *HuffmanNode
-	// token holds what the node represents.
-	// If the node is an internal node, the the token is rune(0), representing '\0'.
-	// Or else the token is representative of a particular token in the document.
-	// Please see Wikipedia for the definition of a token
+	// token stored by a node.
+	// If the node is internal, then the token is rune(0), representing '\0'.
+	// Or else the token is a particular token in the document.
 	token rune
-	// Number of times the token is counted in the document
+	// count is the number of times the token is present in the document
 	count int
 }
 
@@ -30,64 +26,46 @@ func (hn HuffmanNode) hasLeft() bool { return hn.left != nil }
 func (hn HuffmanNode) hasRight() bool { return hn.right != nil }
 
 // ValidToken shows if a token is valid for non-package level access.
-// An invalid token is an implementation detail and should not be visible to users.
 func (hn HuffmanNode) ValidToken() bool { return hn.token != rune(0) }
 
 // Token represented by the HuffmanNode. The field is not modified ever after the creation of the huffman node.
 func (hn HuffmanNode) Token() rune { return hn.token }
 
 // Count is the number of times a rune is present in the document.
-// If the token is a ValidToken, the count is not modified after the creation of the huffman node.
+// If the token is a `ValidToken`, the count is not modified after the creation of the huffman node.
 func (hn HuffmanNode) Count() int { return hn.count }
 
-// HuffmanTree is, guess what, a pointer to the root HuffmanNode of the tree!
-// HuffmanTree is an alias because if it weren't, the spec (in the following line)
+// HuffmanTree is a pointer to the root HuffmanNode of the tree
+// HuffmanTree cannot be a newtype because if it is, the spec (in the following line)
 // https://golang.org/ref/spec#Method_declarations
 // says that it you can't define methods on it.
-// The reason for the outrageous decision is actually pretty logical,
-// see the link below
 // https://groups.google.com/g/golang-nuts/c/qf76N-uDcHA/m/DTCDNgaF_p4J
 type HuffmanTree = *HuffmanNode
 
-// MakeHuffmanTree creates a completely new HuffmanTree from a string
+// MakeHuffmanTree creates a new HuffmanTree from a string
 func MakeHuffmanTree(content string) HuffmanTree {
-	// wordCount will serve as a multiset to count every occurrence of a rune
+	// wordCount is a multiset
 	wordCount := make(map[rune]int)
 
 	for _, char := range content {
 		// If key doesn't exist, the map defaults to returning 0
-		// And since this map is used to count numbers of encounters
-		// Simply put ++ works fine as the value 'inserted' into the map is 1
 		wordCount[char]++
 	}
 
-	// The reason I'm not setting the capacity to length of wordCount
-	// is that if I do so, the next line would have to maintain a separate integer
-	// just to count the index of huffmanList.
-	// Using a preset-length of 0 makes code more readable by using append only
 	list := MakeHuffmanList(0)
 	for word, count := range wordCount {
-		// The left and right nodes are currently nil because they are not yet connected.
-		// Will do so in the next line.
 		list.Append(HuffmanNode{left: nil, right: nil, token: word, count: count})
 	}
 
-	// The list will be built into a min heap
 	heap.Init(&list)
 
-	// Run for list.Len() - 1 iterations
 	for i := list.Len() - 1; i > 0; i-- {
-		// Retrieve the two nodes where the counts are the smallest
-		// Also, I'm pretty sure they are still HuffmanNode's
+		// Retrieve the smallest (in `count`) two nodes
 		first := heap.Pop(&list).(HuffmanNode)
 		second := heap.Pop(&list).(HuffmanNode)
 
-		// Why create a parent with `Count` the sum of its children?
-		// Well, this way the count of the root of a tree
-		// will be the sum of all its leaf nodes,
-		// since all internal nodes being the sum of their children,
-		// will then propagate the sums to their parents.
-		// And the above condition holds for all sub-trees in the tree.
+		// Every parent having `count` the sum of its children's `count`s
+		// will ensure that the node of a tree would be the sum of its leaves.
 		merged := HuffmanNode{
 			left:  &first,
 			right: &second,
@@ -95,19 +73,14 @@ func MakeHuffmanTree(content string) HuffmanTree {
 			count: first.count + second.count,
 		}
 
-		// Then insert the new node back to the original list
-		// The list looks like a list of trees after the first heap.Push
 		heap.Push(&list, merged)
 	}
 
-	// After n-1 merges (remember total=len(list)-1 in the for loop?)
-	// There would only be one element left in the list
+	// After n-1 merges there would only be one element left in the list
 	if list.Len() != 1 {
 		panic("unreachable")
 	}
 
-	// HuffmanTree is a *HuffmanNode
-	// The 'type conversion' is just for readability
 	return HuffmanTree(&list[0])
 }
 
@@ -115,9 +88,7 @@ func MakeHuffmanTree(content string) HuffmanTree {
 func (hf HuffmanTree) Huffman() map[string]string {
 	dict := make(map[string]string)
 
-	// The root node begins with an empty dictionary and no path at all
-	// As the tree is traversed by the following function,
-	// paths are assigned to the node and that is the node's Huffman code!
+	// The root node's path == ""
 	hf.GenerateByPath(dict, "")
 
 	return dict
@@ -125,18 +96,15 @@ func (hf HuffmanTree) Huffman() map[string]string {
 
 // GenerateByPath generates the huffman codes for an existing HuffmanTree
 func (ht HuffmanTree) GenerateByPath(dict map[string]string, path string) {
-	// If hf is an internal node
-	// then there is nothing to store because characters will only be present in the leaf nodes
+	// Tokens will only be present in the leaf nodes
 	if ht.ValidToken() {
-		// type string is easier to display and use
 		token := string(ht.token)
 		dict[token] = path
 	}
 
-	// In a huffman tree the physical layout of the tree is related to how the nodes are named.
 	// A node is named by following the path from the root to it.
-	// Every left visit (visit left child) a '0' is appended to the code
-	// Every right visit (visit right child) a '1' is appended to the code
+	// Every left visit (visit left child) a '0' is appended to the code.
+	// Every right visit (visit right child) a '1' is appended to the code.
 
 	if ht.hasLeft() {
 		ht.left.GenerateByPath(dict, path+"0")
@@ -147,46 +115,36 @@ func (ht HuffmanTree) GenerateByPath(dict map[string]string, path string) {
 	}
 }
 
-// Huffman is a convenient method to create
-// a huffman tree and the compute the huffman codes
-// Basically, it takes in a document string,
-// then compute the huffman codes for you
+// Huffman takes in a document string and compute the huffman codes into a `map[string]string`
 func Huffman(content string) map[string]string {
 	ht := MakeHuffmanTree(content)
 	return ht.Huffman()
 }
 
 // HuffmanList is used for building up the HuffmanTree
-// So it's very short lived. RIP
 type HuffmanList []HuffmanNode
 
-// MakeHuffmanList creates a new HuffmanList with given length.
+// MakeHuffmanList creates a new HuffmanList with given length
 func MakeHuffmanList(length int) HuffmanList { return make(HuffmanList, length) }
 
 // Append adds a new HuffmanNode to HuffmanList
-// The reason copy is used here is that the node is not that big anyways,
-// and it makes the API simpler
 func (hl *HuffmanList) Append(node HuffmanNode) { *hl = append(*hl, node) }
 
 // Index returns the value of HuffmanList at a certain index
-// I created this method because if someone were to extend this package,
-// HuffmanList without this method would be useless
 func (hl HuffmanList) Index(idx int) HuffmanNode { return hl[idx] }
 
 // The following functions Len, Less, Swap are defined on HuffmanList,
 // but if an interface requires these three methods,
 // both HuffmanList and *HuffmanList implement the interface.
-// It's Go's magic.
 // https://golangbyexample.com/pointer-vs-value-receiver-method-golang/
-// So what we effectively that sort package can use both HuffmanList and *HuffmanList,
+// End result is: sort package can use both HuffmanList and *HuffmanList,
 // and heap package can only use *HuffmanList.
 
 // Len is defined for sort, heap package.
 func (hl HuffmanList) Len() int { return len(hl) }
 
 // Less is defined for sort, heap package.
-// It is desired that the heap is a min heap. So Less(i, j) uses the normal`<` operator.
-// It would be clearer why we want a min heap in creating the HuffmanTree.
+// Compare two nodes based on `Count()`
 func (hl HuffmanList) Less(i, j int) bool { return hl[i].count < hl[j].count }
 
 // Swap is defined for sort, heap package.
@@ -198,15 +156,12 @@ func (hl *HuffmanList) Push(elem Any) {
 		*hl = append(*hl, node)
 		return
 	}
-	// Unreachable because I trust the standard library not to do stupid things
 	panic("unreachable")
 }
 
 // Pop is defined for heap package
 func (hl *HuffmanList) Pop() (last Any) {
 	list := *hl
-
-	// The last index
 	index := len(list) - 1
 
 	// Takes out the last element and shorten existing list
--- main.go
+++ main.go
@@ -9,28 +9,23 @@ import (
 	"strings"
 )
 
-// EmptyString is not a string at all
-const EmptyString string = ""
-
 func main() {
 	var err error
 
 	var inputName, outputName string
 
-	flag.StringVar(&inputName, "i", EmptyString, "The file to be encoded into huffman coding")
-	flag.StringVar(&outputName, "o", EmptyString, "The file where encoded strings can be saved")
+	flag.StringVar(&inputName, "i", "", "The file to be encoded into huffman coding")
+	flag.StringVar(&outputName, "o", "", "The file where encoded strings can be saved")
 
 	flag.Parse()
 
 	var inputFile, outputFile *os.File
 
-	// Read the input file, then handle the file to MakeHuffmanTreeFromFile
 	if inputFile, err = os.Open(inputName); err != nil {
 		log.Fatalln("No such input file!")
 	}
 	defer inputFile.Close()
 
-	// Output is a json file.
 	if outputFile, err = os.Create(outputName); err != nil {
 		log.Fatalln("No such output file!")
 	}
@@ -44,23 +39,20 @@ func main() {
 		log.Fatalln("There is something terrifying in the input file!")
 	}
 
-	// Still going to do some processing before using
 	tmpData := string(data)
 
-	// Newlines are not very printable, so I'll just get rid of it.
+	// Newlines are not very printable, so don't care about it.
 	content := strings.ReplaceAll(tmpData, "\n", " ")
 
-	// This does all the heavy lifting.
+	// Compute Huffman codes.
 	codes := Huffman(content)
 
-	// Write in a json format to the output file
 	if data, err = json.Marshal(codes); err != nil {
-		// Come on! It's just a map[string]string to a json string
-		// What could possibly go wrong?
-		log.Fatalln("This is not possible!")
+		log.Fatalln("map[string]string to json fails!")
 	}
 
 	var n int
+
 	if n, err = outputFile.Write(data); err != nil {
 		log.Fatalf("Read %d lines. Buffer overflow!\n", n)
 	}
